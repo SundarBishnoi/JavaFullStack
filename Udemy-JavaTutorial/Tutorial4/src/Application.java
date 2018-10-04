060d5894c2f3c907caf8d2f8dc99c8560ae81468
Sundar Bishnoi <sundarlal2929@gmail.com>
2018-10-04 20:01:37 +0530

    Add files via upload

--- Udemy-JavaTutorial/Tutorial4/src/Application.java
+++ Udemy-JavaTutorial/Tutorial4/src/Application.java
@@ -1,6 +1,10 @@
-
 public class Application {
 	public static void main(String[] args) {
-		
+		int value=0;
+		while(value<10){
+			System.out.println("Hello"+"   " + value);
+			value++;
+		}
 	}
 }
+
