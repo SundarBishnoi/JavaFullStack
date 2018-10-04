6db3ae4c1cc0b82171cb253cb2e5ac0f84c02448
Sundar Bishnoi <sundarlal2929@gmail.com>
2018-10-04 14:26:13 +0530

    Add files via upload

---
+++ Udemy-JavaTutorial/Tutorial1/src/Application.java
@@ -0,0 +1,16 @@
+
+public class Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			int myNum;
+			myNum=100;
+			short a;
+			long b;
+			double c;
+			char pq='r';
+			boolean p=true;
+			byte q=2;
+			float d;
+	}
+}
---
+++ Udemy-JavaTutorial/Tutorial3/src/Application.java
@@ -0,0 +1,15 @@
+
+public class Application {
+			public static void main(String[] args) {
+			int myNum=100;
+			String text="hello";
+			String blank="  ";
+			String name="Dibu";
+			String greeting= text + blank+name;
+			System.out.println(greeting);
+			System.out.println("Hello"+" "+"Dost");
+			System.out.println("My integer is :"+ myNum);
+			double myDouble=7.8;
+			System.out.println("my doubles is :"+ myDouble +".");
+			}
+}
