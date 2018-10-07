1db16b72d19f303d6e0c32a41963205c8ce7d6f0
Sundar Bishnoi <sundarlal2929@gmail.com>
2018-10-08 00:08:35 +0530

    Add files via upload

--- Udemy-JavaTutorial/Tutorial12/src/Application.java
+++ Udemy-JavaTutorial/Tutorial12/src/Application.java
@@ -5,16 +5,29 @@ class Person {
 	// classes can contain
 	// 1.Data
 	// 2.Subroutines
+	void speak() {
+		for(int i=0;i<2;i++) {
+			System.out.println("This is "+ name +" and " + age +" years old.");
+		}
+	}
+	void sayHello() {
+		System.out.println("Hello There!");
+	}
 }
 
 public class Application {
-
 	public static void main(String[] args) {
 		Person person1 = new Person();
 		person1.name = "Sundar";
+		person1.age =21;
 		System.out.println(person1.name);
+		person1.speak();
+		person1.sayHello();
 		Person person2 = new Person();
-		person2.age = 20;
+		person2.name = "bishnoi";
+		person2.age = 21;
+		person2.speak();
+		person2.sayHello();
 		System.out.println(person2.age);
 	}
 }
